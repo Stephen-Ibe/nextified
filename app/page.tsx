f4e3e9f3314f62e8aa9cb09d8e88f4ea1e79f028
Stephen-Ibe <saintkalson@gmail.com>
2023-06-28 10:50:30 +0100

    page heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex-col w-full text-center flex-center">
+      <h1 className="head_text">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompt</span>
+      </h1>
+      <p className="desc">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
 };
 
 export default Home;
