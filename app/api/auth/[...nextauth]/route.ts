c8578dd336fcb7fc52114d3a6cb095a88eb79f63
Stephen-Ibe <saintkalson@gmail.com>
2023-06-30 20:06:15 +0100

    next-auth provider config

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log(user, account, profile, email, credentials);
+      return true;
+    },
+    async session({ session, token, user }) {
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
